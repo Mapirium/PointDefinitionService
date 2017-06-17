70114dc79ab3f281b7978c3469f4a1848d345c66
Stefan Urech <stefan@surech.ch>
2017-06-17 11:15:38 +0200

    stu: Eine Liste im korrekten HAL-Format zurückgeben

---
+++ src/main/java/ch/mapirium/server/pointdefservice/rest/model/PointDefinitionListResource.java
@@ -0,0 +1,9 @@
+package ch.mapirium.server.pointdefservice.rest.model;
+
+import ch.mapirium.server.common.springmvc.rest.HalResource;
+
+/**
+ * Hülle für eine Liste von Punkt-Definitionen
+ */
+public class PointDefinitionListResource extends HalResource {
+}
